8a05c9f120e0de1fe103e7003fa5abff7954f925
daxbrat <dax.dacosta@gmail.com>
2022-12-20 14:30:32 -0600

    create function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+let wordResultArr = [];
+const reverseString = function (normalWord) {
+  wordResultArr = normalWord.split(""); // [h,e,l,l,o, ,w,o,r,l,d]
 };
 
 // Do not edit below this line
