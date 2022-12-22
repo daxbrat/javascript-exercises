fc97bb46be41cc7d4f8a07eea354c433eb6b1f34
daxbrat <dax.dacosta@gmail.com>
2022-12-22 09:32:01 -0600

    create removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (originalArr, ...valuesToRemove) {
+  return originalArr.filter((value) => !valuesToRemove.includes(value));
 };
 
 // Do not edit below this line
