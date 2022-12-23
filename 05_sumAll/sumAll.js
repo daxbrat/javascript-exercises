10084a71fa0e0c2575e76e699dd5ef99dd334ead
daxbrat <dax.dacosta@gmail.com>
2022-12-23 15:04:51 -0600

    create sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+let sum = 0;
+const sumAll = function (lowNum, highNum) {
+  if (highNum < lowNum) {
+    const numHolder = lowNum;
+    lowNum = highNum;
+    highNum = numHolder;
+  }
+  for (let i = lowNum; i <= highNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
