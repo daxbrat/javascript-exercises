af2d341b77255202b7df42f3f44d20931e5e07f5
daxbrat <dax.dacosta@gmail.com>
2022-12-23 15:14:34 -0600

    complete sumAll function and tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -5,6 +5,12 @@ const sumAll = function (lowNum, highNum) {
     lowNum = highNum;
     highNum = numHolder;
   }
+  if (lowNum < 0 || highNum < 0) {
+    return "ERROR";
+  }
+  if (typeof lowNum != "number" || typeof highNum != "number") {
+    return "ERROR";
+  }
   for (let i = lowNum; i <= highNum; i++) {
     sum += i;
   }
