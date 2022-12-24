6b9751a4c8af4496b364f3d4e92c3e2d72d18585
daxbrat <dax.dacosta@gmail.com>
2022-12-23 18:49:49 -0600

    complete conversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
 const convertToCelsius = function(tempFah) {
+  return Math.round(tempFah - 32) * (5/9);
 };
 
 const convertToFahrenheit = function(tempCel) {
+  return Math.round(tempCel * (9/5)) + 32;
 };
 
 // Do not edit below this line
