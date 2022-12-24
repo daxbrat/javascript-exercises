22485b5a359ed0e04d4802fbeb4b6637723d938d
daxbrat <dax.dacosta@gmail.com>
2022-12-24 17:14:02 -0600

    complete conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const convertToCelsius = function(tempFah) {
-  return Math.round(tempFah - 32) * (5/9);
+const convertToCelsius = function (tempFah) {
+  let tempF = (tempFah - 32) * (5 / 9);
+  tempF = Math.round(tempF * 10) / 10;
+  return tempF;
 };
 
-const convertToFahrenheit = function(tempCel) {
-  return Math.round(tempCel * (9/5)) + 32;
+const convertToFahrenheit = function (tempCel) {
+  let tempC = tempCel * (9 / 5) + 32;
+  tempC = Math.round(tempC * 10) / 10;
+  return tempC;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
